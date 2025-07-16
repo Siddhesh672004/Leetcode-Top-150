cc3275417b7fcacfa046d99c28fcf4e5ca9dd3f8
Siddhesh Avinash Chaudhari <122895289+Siddhesh672004@users.noreply.github.com>
2025-07-16 13:58:33 +0530

    Time: 0 ms (100%), Space: 28.1 MB (85.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,34 +1,61 @@
+//Optimal Solution
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
-            int count = 0;
-            int ele;
-            int n = nums.size();
+        int count = 0;
+        int ele;
+        int n = nums.size();
 
-            for(int i=0; i<n; i++){
-                if(count == 0){
-                    count++;
-                    ele = nums[i];
-                }
-                else if(ele == nums[i]){
-                    count++;
-                }
-                else{
-                    count--;
-                }
+        for(int i=0; i<n; i++){
+            if(count == 0){
+                count++;
+                ele = nums[i];
             }
+            else if(nums[i] == ele){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
 
-            int count1 = 0;
-            for(int i=0; i<n; i++){
-                if(ele == nums[i]){
-                    count1++;
-                }
+        int count1 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == ele){
+                count1++;
             }
+        }
 
-            if(count1 > n/2){
-                return ele;
-            }
-            return -1;
-        }      
+        if(count1 > n/2){
+            return ele;
+        }
+
+        return -1;
+    }
 };
+
+//Better Solution 
+//Time Complexity: O(n log n) + O(n)
+//Space Complexity: O(n)
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+
+//         map <int, int> mp;
+//         int n = nums.size();
+//         for(int i=0; i<n; i++){
+//             mp[nums[i]]++;
+//         }
+
+//         for(auto it : mp){
+//             if(it.second > n/2){
+//                 return it.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
