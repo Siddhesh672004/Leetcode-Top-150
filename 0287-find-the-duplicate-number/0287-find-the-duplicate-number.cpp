a9dd17d327ebf65329e20916b751462734bf3d33
Siddhesh Avinash Chaudhari <122895289+Siddhesh672004@users.noreply.github.com>
2025-07-31 01:02:54 +0530

    Time: 0 ms (100%), Space: 65.1 MB (42.64%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,13 +1,44 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        
+        int ans;
 
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i] == nums[i+1]){
-                return nums[i];
+        for(int i=0; i<nums.size(); i++){
+            int index = abs(nums[i]);
+            if(nums[index] < 0){
+                ans = index;
+                break;
             }
+
+            nums[index] *= -1;
         }
-        return -1;
+        return ans;
     }
 };
+
+
+
+
+
+
+
+// //Time Complexity: O(n log n)
+// //Space Complexity: O(n)
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+
+//         for(int i=0; i<nums.size()-1; i++){
+//             if(nums[i] == nums[i+1]){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// };
