8363d02f4509c246a244fae583801b40e0939216
Siddhesh Avinash Chaudhari <122895289+Siddhesh672004@users.noreply.github.com>
2025-07-30 19:31:12 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.93%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,38 +1,91 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
+        int l = 0;
+        int m = 0;
+        int r = nums.size()-1;
 
-        int zeros = 0;
-        int ones = 0;
-        int twos = 0;
-
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == 0){
-                zeros++;
+        while(m <= r){
+            if(nums[m] == 0){
+                swap(nums[m], nums[l]);
+                l++;
+                m++;
             }
-            else if(nums[i] == 1){
-                ones++;
+            else if(nums[m] == 1){
+                m++;
             }
             else{
-                twos++;
+                swap(nums[m], nums[r]);
+                r--;
             }
-        }
-
-        int i=0;
-        while(zeros--){
-            nums[i] = 0;
-            i++;
-        }
-
-        while(ones--){
-            nums[i] = 1;
-            i++;
-        }
-
-        while(twos--){
-            nums[i] = 2;
-            i++;
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         // sort(nums.begin(), nums.end());
+
+//         //Time Complexity: O(n)
+//         //Space Complexity: O(1)
+
+//         int zeros = 0;
+//         int ones = 0;
+//         int twos = 0;
+
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i] == 0){
+//                 zeros++;
+//             }
+//             else if(nums[i] == 1){
+//                 ones++;
+//             }
+//             else{
+//                 twos++;
+//             }
+//         }
+
+//         int i=0;
+//         while(zeros--){
+//             nums[i] = 0;
+//             i++;
+//         }
+
+//         while(ones--){
+//             nums[i] = 1;
+//             i++;
+//         }
+
+//         while(twos--){
+//             nums[i] = 2;
+//             i++;
+//         }
+//     }
+// };
