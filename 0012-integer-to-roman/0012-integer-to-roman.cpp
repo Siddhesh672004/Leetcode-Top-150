6c684063479d4902b5332bc1cd87ceddb350513f
Siddhesh Avinash Chaudhari <122895289+Siddhesh672004@users.noreply.github.com>
2025-08-07 23:38:15 +0530

    Time: 3 ms (63.22%), Space: 9.2 MB (77.71%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -5,13 +5,11 @@ public:
                                 "I"};
         int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         string ans = "";
-
-        for(int i=0; i<13; i++)
-        {
-            while(num >= values[i])
-            {
-                ans+=romanSymbols[i];
-                num-=values[i];
+        
+        for(int i=0; i<13; i++){
+            while(num >= values[i]){
+                ans += romanSymbols[i];
+                num -= values[i];
             }
         }
         return ans;
