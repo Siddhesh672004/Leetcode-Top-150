edced6e8d35e84dec67cba03acf551cb8cb201e8
Siddhesh Avinash Chaudhari <122895289+Siddhesh672004@users.noreply.github.com>
2025-08-14 22:54:29 +0530

    Time: 30 ms (36.81%), Space: 65.2 MB (42.97%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,62 +1,13 @@
-//Time Complexity: O(n)
-//Space Complexity: O(1)
-
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
-        while(nums[0] != nums[nums[0]]){
-            swap(nums[0], nums[nums[0]]);
-        }
+        sort(nums.begin(), nums.end());
 
-        return nums[0];
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i] == nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
     }
 };
-
-
-
-
-//Time Complexity: O(n)
-//Space Complexity: O(1)
-
-// class Solution {
-// public:
-//     int findDuplicate(vector<int>& nums) {
-        
-//         int ans;
-
-//         for(int i=0; i<nums.size(); i++){
-//             int index = abs(nums[i]);
-//             if(nums[index] < 0){
-//                 ans = index;
-//                 break;
-//             }
-
-//             nums[index] *= -1;
-//         }
-//         return ans;
-//     }
-// };
-
-
-
-
-
-
-
-// //Time Complexity: O(n log n)
-// //Space Complexity: O(n)
-
-// class Solution {
-// public:
-//     int findDuplicate(vector<int>& nums) {
-//         sort(nums.begin(), nums.end());
-
-//         for(int i=0; i<nums.size()-1; i++){
-//             if(nums[i] == nums[i+1]){
-//                 return nums[i];
-//             }
-//         }
-//         return -1;
-//     }
-// };
