4f3fcf5e833a1e13743b4756650118a88ca84b33
Siddhesh Chaudhari <122895289+Siddhesh672004@users.noreply.github.com>
2025-11-21 11:49:27 +0530

    Time: 0 ms (100%), Space: 63.5 MB (78.18%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,26 @@
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int findPeakIndex(vector<int> &arr){
+        int start = 0;
+        int end = arr.size()-1;
+
+        while(start < end) {
+            int mid = start + (end-start)/2;
+
+            if(arr[mid] < arr[mid+1]) {
+                start = mid + 1;
+            }    
+            else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    int peakIndexInMountainArray(vector<int>& arr) {
+        return findPeakIndex(arr);
+    }
+};
