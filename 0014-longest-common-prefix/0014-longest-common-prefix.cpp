b6d89195e6af7261042ae5e91037432f2128578d
Siddhesh Chaudhari <122895289+Siddhesh672004@users.noreply.github.com>
2026-01-05 18:12:46 +0530

    Time: 0 ms (100%), Space: 12.1 MB (17.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,38 +1,64 @@
-//Time Complexity: O(n)
-//Space Complexity: O(n)
+//Time Complexity: O(n * m)
+//Space Complexity: O(1)
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans;
-        int i = 0;
+        int n = strs.size();
+        // step 1: Sort the s 
+        sort(strs.begin(), strs.end());
 
-        while(true){
-            char curr_ch = 0;
+        string s1 = strs[0];
+        string s2 = strs[n-1];
 
-            for(auto str : strs){
-
-                if(i >= str.size()){
-                    //Out of bound
-                    curr_ch = 0;
-                    break;
-                }
-                
-                //Just Started
-                if(curr_ch == 0){
-                    curr_ch = str[i];
-                }
-                else if(curr_ch != str[i]){
-                    curr_ch = 0;
-                    break;
-                }
+        for(int i=0; i<s1.length(); i++) {
+            if(s1[i] != s2[i]) {
+                return s1.substr(0, i);
             }
-            if(curr_ch == 0){
-                break;
-            }
-            ans.push_back(curr_ch);
-            i++;
         }
-        return ans;
+
+        return s1;
     }
 };
+
+
+
+
+// //Time Complexity: O(n)
+// //Space Complexity: O(n)
+
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         string ans;
+//         int i = 0;
+
+//         while(true){
+//             char curr_ch = 0;
+
+//             for(auto str : strs){
+
+//                 if(i >= str.size()){
+//                     //Out of bound
+//                     curr_ch = 0;
+//                     break;
+//                 }
+                
+//                 //Just Started
+//                 if(curr_ch == 0){
+//                     curr_ch = str[i];
+//                 }
+//                 else if(curr_ch != str[i]){
+//                     curr_ch = 0;
+//                     break;
+//                 }
+//             }
+//             if(curr_ch == 0){
+//                 break;
+//             }
+//             ans.push_back(curr_ch);
+//             i++;
+//         }
+//         return ans;
+//     }
+// };
